35bdabc970a2846405944a93ed49da42bac171d2
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-03-09 12:29:57 +0100

    ball can hit every side of player and blocks. Some issues if player hit ball left/right side. Very rarely ball leave game-board. Anyway... works, v0.2

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,18 @@ const player = document.getElementById('player');
 const arkanoid = document.getElementById('arkanoid');
 const canvasBall = document.getElementById('ball');
 const ctx = canvasBall.getContext("2d");
+const blockWH = document.getElementById('wh');
 
 const 
 balls = [],
 randomInit = Math.random()-Math.random(),
-speed = 3,
+speed = 5,
 size = 5,
 amount = 1,
 color = 'hsl(200, 40%, 50%)';
 
+const blockWidth = blockWH.offsetWidth;
+const blockHeight = blockWH.offsetHeight;
 const playerWidth = player.offsetWidth;
 const playerHeight = player.offsetHeight;
 const playerHeight2 = player.offsetHeight*2;
@@ -20,21 +23,6 @@ const h = canvasBall.height = arkaBoard.offsetHeight;
 ctx.fillStyle = color;
 
 let x = 0;
-
-// const boxes = [
-//   ['#arka-map', 60] // 5x12
-// ];
-
-// for (const [map, items] of boxes) {
-//   const container = document.querySelector(map);
-
-//   for (let i=0; i<items; i++){
-//     const newBlock = document.createElement('div');
-//     newBlock.className = 'block';
-//     newBlock.id = i;
-//     container.appendChild(newBlock);
-//   }
-// };
 
 const boxes = [
   ['#arka-map', 12, 5] // 12 x 5
@@ -48,17 +36,14 @@ for (const [map, columns, rows] of boxes) {
       const newBlock = document.createElement('div');
       newBlock.className = 'block';
       // newBlock.style.transform = `translate(${j*playerWidth}px, ${i*playerHeight}px)`;
-      newBlock.style.left = j * playerWidth + 'px';
-      newBlock.style.top = i * playerHeight + 'px';
+      newBlock.style.left = j * blockWidth + 'px';
+      newBlock.style.top = i * blockHeight + 'px';
       container.appendChild(newBlock);
     }
   }
 };
 
-
-
 const blocks = document.querySelectorAll('.block');
-
 
 const movePlayer = ()=>{
 
@@ -67,18 +52,12 @@ const movePlayer = ()=>{
   
   if (x <= 0) {
     x = 0
-    // player.style.transform = `translateX(${x}px)`;
   } else if (x >= (w - playerWidth)) {
     x = w - playerWidth
-    // player.style.transform = `translateX(${x}px)`;
-  } 
-  // else {
-  //   player.style.transform = `translateX(${x}px)`;
-  // }
+  }
   player.style.transform = `translateX(${x}px)`;
   document.getElementById('test').innerHTML = "x = "+x;
-  player.innerHTML = "x = "+x; 
-  
+  // player.innerHTML = "x = "+x;   
 };
 
 
@@ -120,64 +99,99 @@ class Ball {
       console.log('hit top');      
     }
 
-    // hit player
-    // if ( this.xpos > x && this.xpos < (x + playerWidth) ) {
-    //   if ( 
-    //   (this.ypos + this.size) >= (h - playerHeight2) 
-    //   // || (this.ypos + this.size) == (h - playerHeight/2) 
-    //   ) {
-    //     this.dy = -this.dy;
-    //     console.log('hit playerY'); 
-    //   }
-    // }
-
-    if ( 
-      this.xpos > x && 
-      this.xpos < (x + playerWidth) &&
-      this.ypos + this.size > (h - playerHeight2) &&
-      this.ypos + this.size < (h + playerHeight)      
+    // hit player    
+    if ( // hits Y axis
+    this.xpos > x && 
+    this.xpos < (x + playerWidth) 
+    ) {
+      if (
+      this.ypos == (h - playerHeight2)
       ) {
         this.dy = -this.dy;
-        console.log('hit playerY'); 
-
-
-        // if ( 
-        // (this.ypos + this.size) >= (h - playerHeight2) || 
-        // (this.ypos + this.size) <= (h + playerHeight)  
-        // ) {
-
-        // }
-        // if ( 
-        // (this.xpos + this.size) >= x &&
-        // (this.xpos + this.size) <= (x + playerWidth)
-        // ) {
-        //   this.dx = -this.dx;
-        //   console.log('hit playerX'); 
-        // }
+        console.log('hit player top');
       }
-
-
+      if (
+      this.ypos >= (h - playerHeight) &&
+      this.ypos - this.size <= (h - playerHeight)
+      ) {
+        this.dy = -this.dy;
+        console.log('hit player bot');
+      }
+    }
+    
+    if ( // hit X axis
+    this.ypos > (h - playerHeight2) &&
+    this.ypos < (h - playerHeight)
+    ) {
+      if (
+      this.xpos + this.size >= x &&
+      this.xpos <= x
+      ) {
+        this.xpos = x - this.size*2;
+        this.dx = -this.dx;
+        this.dy = -this.dy;
+        console.log('hit player left');
+      }
+      if (
+      this.xpos - this.size <= (x + playerWidth) &&
+      this.xpos >= (x + playerWidth)
+      ) {
+        this.xpos = (x + playerWidth) + this.size*2;
+        this.dx = -this.dx;
+        this.dy = -this.dy;
+        console.log('hit player right');
+      }
+      if (
+      this.xpos - this.size > x &&
+      this.xpos < (x + playerWidth/2)
+      ) {
+        this.ypos = (h - playerHeight2) - this.size
+        this.xpos = x - this.size*3;
+        console.log('ball inside left');
+      }
+      if (
+      this.xpos + this.size < (x + playerWidth) &&
+      this.xpos > (x + playerWidth/2)
+      ) {
+        this.ypos = (h - playerHeight2) - this.size
+        this.xpos = (x + playerWidth) + this.size*3;        
+        console.log('ball inside right');
+      }
+    }
   
     // hit blocks
-
     for (let i=0; i<blocks.length; i++) {
       const blockX = blocks[i].offsetLeft;
       const blockY = blocks[i].offsetTop;
       const blockW = blocks[i].offsetWidth;
       const blockH = blocks[i].offsetHeight;
       if ( 
-        (this.ypos - this.size) <= blockY + blockH &&
-        this.xpos > blockX && this.xpos < (blockX + blockW)
+      this.xpos >= blockX && 
+      this.xpos <= (blockX + blockW) 
+      ) {
+        if (          
+        (this.ypos + this.size) >= blockY &&
+        (this.ypos - this.size) <= (blockY + blockH)
         ) {
-        this.dy = -this.dy;
-        console.log('hit blockY');       
-        
-        // blocks[i].classList.remove('block');
-        // blocks[i].classList.add('hidden');
-        blocks[i].remove();
-      }          
+          this.dy = -this.dy;
+          console.log('hit block Y');
+          blocks[i].remove();          
+        }        
+      }
+      if ( 
+      this.ypos >= blockY &&
+      this.ypos <= (blockY + blockH)
+      ) {
+        if (
+          (this.xpos + this.size) >= blockX && 
+          (this.xpos - this.size) <= (blockX + blockW) 
+        ) {
+          this.dx = -this.dx;
+          console.log('hit block X');
+          blocks[i].remove(); 
+        }  
+      }
     }
-
 
     // move ball
     this.xpos += this.dx;
@@ -207,8 +221,4 @@ const moveBalls = () => {
 
 createBalls();
 
-
 document.addEventListener('mousemove', movePlayer);
-
-
-
